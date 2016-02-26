e11bc3b52f25f84cc5e1fd05004c27c5f57c1c6b
Your Name <joe.scalera@gmail.com>
2016-02-26 15:05:11 -0800

    service hw and master fix

--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -9,7 +9,7 @@
     year: '1881',
     albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
-        { name: 'Blue', length: '161.71', audioUrl: '/assets/music/blue' },
+         { name: 'Blue', length: '161.71', audioUrl: '/assets/music/blue' },
          { name: 'Green', length: '103.96', audioUrl: '/assets/music/green' },
          { name: 'Red', length: '268.45', audioUrl: '/assets/music/red' },
          { name: 'Pink', length: '153.14', audioUrl: '/assets/music/pink' },
@@ -34,7 +34,13 @@ var albumMarconi = {
          Fixtures.getAlbum = function() {
          return albumPicasso;
      };
-
+         Fixtures.getCollection = function(numberOfAlbums) {
+           var  numberOfAlbums = [];
+             for (var i = 0; i < 12; i++) {
+                 numberOfAlbums.push(angular.copy(albumPicasso));
+             }
+             return numberOfAlbums;
+         }
          return Fixtures;
      }
  
